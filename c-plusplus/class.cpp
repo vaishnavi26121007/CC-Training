83ecaf313ce4514ce727469d6122bb3d28dbc618
vaishnavi26121007 <vaishnaviikrishnan1012@gmail.com>
2026-07-22 22:33:37 +0530

    Added C++ Programs

---
+++ c-plusplus/class.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+class abc
+{
+   public:
+   void operations(int a,int b)
+   {
+    cout<<a+b<<std::endl;
+   }
+};
+class abcd:public abc
+{
+    public:
+    void operation(int a,int b)
+    {
+     cout<<abs(a-b)<<std::endl;
+    }
+};
+int main()
+{
+    abcd it;
+    it.operation(5,10);
+    it.operation(13,7);
+}
